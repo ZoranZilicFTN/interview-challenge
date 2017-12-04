45fb1742121a065b38fcbc03f0652217fc43d8af
ZoranZilicFTN <zzftn90@gmail.com>
2017-12-04 14:12:56 +0100

    I started working on the problem
    Looping through the array to find 2 highest bars on each side

---
+++ Project pleo/src/Base/Index.java
@@ -0,0 +1,25 @@
+package Base;
+
+public class Index {
+
+	public static void main(String[] args) {
+		System.out.println("Let's start");
+		
+		//I need to make an array of 10 fields with values of bar-height
+		int arr[] = new int[]{2, 5, 1, 3, 1, 2, 1, 7, 7, 6};
+		
+		//I need to find highest bar on the left and on the right
+		//find highest on left
+		
+		//find highest on right
+		
+				
+		//Water will fill TWO-BARS-DISTANCE x HEIGHT-OF-LOWER-BAR
+		
+		//the surface minus values in arrays in between = the surface of water
+		
+		
+
+	}
+
+}
