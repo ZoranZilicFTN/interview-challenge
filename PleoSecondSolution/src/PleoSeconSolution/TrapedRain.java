9f0e10340cc4500726c296bf1a446397fc8e500f
ZoranZilicFTN <zzftn90@gmail.com>
2017-12-04 17:16:03 +0100

    Main file and class with the tast
    pulling out the greatest element.

--- PleoSecondSolution/src/PleoSeconSolution/TrapedRain.java
+++ PleoSecondSolution/src/PleoSeconSolution/TrapedRain.java
@@ -1,5 +1,7 @@
 package PleoSeconSolution;
 
+import java.util.Arrays;
+
 public class TrapedRain {
 	//constructor
 	TrapedRain(){}
@@ -9,11 +11,16 @@ public class TrapedRain {
 		//Vector : [2, 5, 1, 3, 1, 2, 1, 7, 7, 6]
 		int arr[]    = {2,5,1,3,1,2,1,7,7,6};
 		int n	     = arr.length-1;
-		int newArr[] = {5,5,5,5,5,5,5,5,5,5,};
+		int newArr[] = new int[arr.length];
 		
+		//biggest value, and first biggest on the left
 		int firstBig  = 0;
 		int secondBig = 0;
 		
+		//pulling out the Biggest value #1 I sorted, #2 I took last element
+		int sortArray[] = arr.clone();
+	    Arrays.sort(sortArray);
+	    secondBig = sortArray[n];
 		
 		// #1 chopping the left part (every element that's smaller than first biggest will be equal to the biggest
 		for (int i=1; i<=n; i++) {	
@@ -22,23 +29,24 @@ public class TrapedRain {
 				firstBig=arr[1];
 			}
 			
-			if(arr[i-1]<arr[i] && arr[i]>=firstBig && arr[i]!=7) {
+			if(arr[i-1]<arr[i] && arr[i]>=firstBig && arr[i]!=secondBig) {
 				arr[i-1]=arr[i];
 			}else {
 				arr[i-1]=arr[i-1];
 			}
 			
-			if(arr[i]==7) {
-				secondBig=i-1;
-			}
 		}
-		
 		
 		// #2 Chopping the right part
 		for (int i=secondBig; i<=n; i++) {	
 			arr[i]=firstBig;
 		}
 		int result = 0;
+		
+		//array of 10 elements all equal to firstBig
+		for	(int i=0; i<=n; i++) {
+			newArr[i]=firstBig;
+		}
 		
 		for (int i=0; i<n; i++) {
 			int sub = newArr[i]-arr[i];
